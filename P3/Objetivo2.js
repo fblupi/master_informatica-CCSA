0e63cf0881f1d408e098d70b05d0093543dee7e7
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-04-19 19:47:39 +0200

    Ejercicio 4 parcialmente hecho Objetivo 2 #17

--- P3/Objetivo2.js
+++ P3/Objetivo2.js
@@ -7,8 +7,10 @@ db.pedidos.count()
 db.pedidos.find({"Localidad": "Jaen"}).count()
 
 // 3. Facturación total de clientes por localidad
-db.pedidos.aggregate([{$group: {"_id": "$Localidad", facturacion_total: {$sum : "$Facturacion"}}}])
+db.pedidos.aggregate([{$group: {"_id": "$Localidad", facturacion_total: {$sum: "$Facturacion"}}}])
 
 // 4. Facturación media de clientes por localidad para las localidades distintas a "Jaen" con facturación media mayor de 5000. Ordenación por Localidad descendente. Eliminar el _id y poner el nombre en mayúsculas.
+// !!! Queda lo de eliminar el _id y poner el nombre en mayúsculas
+db.pedidos.aggregate([{$group: {"_id": "$Localidad", facturacion_media: {$avg: "$Facturacion"}}}, {$match: {"_id": {$ne: "Jaen"}, facturacion_media: {$gt: 5000}}}, {$sort: {"_id": -1}}])
 
 // 5. Calcula la cantidad total facturada por cada cliente (uso de "unwind")
