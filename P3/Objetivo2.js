06eecaae9318ad0d3b3560a53484dd911eaa5a68
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-04-22 13:10:01 +0200

    Objetivo 1 y ejercicios 1-3 del objetivo 2 en la memoria #19

--- P3/Objetivo2.js
+++ P3/Objetivo2.js
@@ -1,16 +1,35 @@
-// A partir de la colección pedidos utilizaremos consultas más complejas por medio de los operadores de agregación (pipeline). Por facilidad se indica la consulta en formato SQL estándar. Las tareas a realizar en este caso obtener:
+// A partir de la colección pedidos utilizaremos consultas más complejas por 
+// medio de los operadores de agregación (pipeline). Por facilidad se indica la
+// consulta en formato SQL estándar. Las tareas a realizar en este caso obtener:
 
 // 1. Nº total de clientes
-db.pedidos.count()
+
+db.pedidos.count();
 
 // 2. Nº total de clientes de Jaén
-db.pedidos.find({"Localidad": "Jaen"}).count()
+
+db.pedidos.find(
+    {"Localidad": "Jaen"}
+).count();
 
 // 3. Facturación total de clientes por localidad
-db.pedidos.aggregate([{$group: {"_id": "$Localidad", facturacion_total: {$sum: "$Facturacion"}}}])
 
-// 4. Facturación media de clientes por localidad para las localidades distintas a "Jaen" con facturación media mayor de 5000. Ordenación por Localidad descendente. Eliminar el _id y poner el nombre en mayúsculas.
-// !!! Queda lo de eliminar el _id y poner el nombre en mayúsculas
-db.pedidos.aggregate([{$group: {"_id": "$Localidad", facturacion_media: {$avg: "$Facturacion"}}}, {$match: {"_id": {$ne: "Jaen"}, facturacion_media: {$gt: 5000}}}, {$sort: {"_id": -1}}])
+db.pedidos.aggregate(
+    [
+        {
+            $group: 
+            {
+                "_id": "$Localidad", 
+                facturacion_total: {$sum: "$Facturacion"}
+            }
+        }
+    ]
+);
+
+// 4. Facturación media de clientes por localidad para las localidades 
+// distintas a "Jaen" con facturación media mayor de 5000. Ordenación por 
+// Localidad descendente. Eliminar el _id y poner el nombre en mayúsculas.
+
 
 // 5. Calcula la cantidad total facturada por cada cliente (uso de "unwind")
+
