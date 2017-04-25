3d901a55f57656d66e7c2b4f3103e2939da4da09
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-04-25 21:02:58 +0200

    Ejercicio1 Objetivo 3

--- P3/Objetivo3.js
+++ P3/Objetivo3.js
@@ -13,6 +13,71 @@
 //    mediante un enfoque MapReduce conforme a los pasos que se ilustran en el 
 //    tutorial práctico
 
+db.runCommand({
+    mapReduce: "cities",
+    map: function Map() {
+        emit(
+            this.CountryID,
+            {
+                "data":
+                [
+                    {
+                        "city": this.City,
+                        "lat": this.Latitude,
+                        "lon": this.Longitude
+                    }
+                ]
+            }
+        );
+    },
+    reduce: function Reduce(key, values) {
+        var reduced = { "data": [] };
+        for (var i in values) {
+            var inter = values[i];
+            for (var j in inter.data) {
+                reduced.data.push(inter.data[j]);
+            }
+        }
+        return reduced;
+    },
+    finalize: function Finalize(key, reduced) {
+        if (reduced.data.length == 1) {
+            return {
+                "message": "Este país solo contiene una ciudad"
+            };
+            var min_dist = 999999999999;
+            var city1 = { "name": "" };
+            var city2 = { "name": "" };
+            var c1;
+            var c2;
+            var d2;
+            for (var i in reduced.data) {
+                for (var j in reduced.data) {
+                    if (i >= j) {
+                        continue;
+                    }
+                    c1 = reduced.data[i];
+                    c2 = reduced.data[j];
+                    d2 = (c1.lat - c2.lat) * (c1.lat - c2.lat) + (c1.lon - c2.lon) * (c1.lon - c2.lon);
+                    if (d2 < min_dist && d2 > 0) {
+                        min_dist = d2;
+                        city1 = c1;
+                        city2 = c2;
+                    }
+                }
+            }
+            return {
+                "city1": city1.name,
+                "city2": city2.name,
+                "dist": Math.sqrt(min_dist)
+            };
+        }
+    },
+    out: {
+        merge: "ciudades_proximas"
+    }
+});
+
 // 2. ¿Cómo podríamos obtener las ciudades más distintas en cada país?
 
 // 3. ¿Qué ocurre si en un país hay dos parejas de ciudades que están a la 
